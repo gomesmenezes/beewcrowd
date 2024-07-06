bb30413acb975a7bfa65dda94e628dfc2a7860cc
José Gomes de Menezes <josegomesd6@gmail.com>
2024-07-06 16:31:13 -0300

    Salary

---
+++ beginner/salary.py
@@ -0,0 +1,8 @@
+employee = int(input(''))
+workedMonth  = int(input(''))
+perHour = float(input(''))
+
+salary = workedMonth * perHour
+
+print('NUMBER = ' + str(employee))
+print(f"SALARY = U$ {salary:.2f}") 
